eb390c8a53af1511309fccc61e205b9e9cbd9622
Jordan <jordinholenhart@gmail.com>
2025-05-15 19:30:35 -0700

    functions have been added for fetching the API. switched from axios bc it was giving me a hard time, just using normal try-catch logic. going to put all fetching functions here, all styling script will be in main.js

--- src/fetch.js
+++ src/fetch.js
@@ -1,14 +1,36 @@
-import axios from 'axios';
+const table = document.getElementById('table');
 
-const url = 'https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League';
+async function loadTeams() {
+  try {
+    const response = await fetch('https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League');
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
 
-axios.get(url)
-  .then(response => {
-    const teams = response.data.teams;
+    const data = await response.json();
+    const teams = data.teams.slice(0, 20);
+
     teams.forEach(team => {
-      console.log(team.strTeam);
+      const cell = document.createElement('div');
+      cell.className = 'cell';
+
+      const img = document.createElement('img');
+      img.src = team.strBadge;
+      img.alt = team.strTeam;
+
+      const teamName = document.createElement('h3')
+      teamName.textContent = team.strTeam;
+
+
+      cell.appendChild(img);
+      cell.appendChild(teamName);
+      table.appendChild(cell);
     });
-  })
-  .catch(error => {
+  } catch (error) {
     console.error('Error fetching teams:', error);
-  });
+  }
+}
+
+loadTeams();
+
